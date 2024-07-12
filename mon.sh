ae8da816e21f801338c51689dbaec829069af1db
Jacek <157295096+mixi23@users.noreply.github.com>
2024-07-13 01:07:49 +0200

    Create mon.sh

---
+++ mon.sh
@@ -0,0 +1,9 @@
+gitconfig --global user.email "jacekdut@o2.pl"
+git config --global user.name "Jacek"
+git init
+git add komendy.sh
+git commit -m "Pierwszy commit - dodanie pliku komendy.sh"
+git push --set-upstream https://github.com/mixi23/lab3 master jacekdut@o2.pl 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
+git add komendy.sh
+git commit -m "Tym razem zmieniono to i to..."
+git push
